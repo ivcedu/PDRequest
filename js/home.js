51f4b9669f8e16cd7c435b2af5510d61f5ba78b5
rich <rich@10.161.54.130>
2015-10-28 10:51:47 -0700

    update AD login

--- js/home.js
+++ js/home.js
@@ -133,7 +133,8 @@ $(document).ready(function() {
             else if ((hrs_step === "Pre-activity" && hrs_status === "Approved") 
                     || (hrs_step === "Pre-activity" && hrs_status === "More Information")
                     || (hrs_step === "Post-activity" && hrs_status === "More Information")
-                    || (reimb_step === "Pre-activity" && reimb_status === "Approved") 
+                    || (reimb_step === "Pre-activity" && reimb_status === "Approved")
+                    || (reimb_step === "Pre-activity" && reimb_status === "Approved Pending Funds") 
                     || (reimb_step === "Pre-activity" && reimb_status === "More Information")
                     || (reimb_step === "Post-activity" && reimb_status === "More Information")) {
                 window.open('postPDRequest.html', '_self');
